c9ae8cf456322af36702484b8d85cf307fa34b4a
alchristophergwapo <alchristopheroppa0143gmail.com>
2019-08-31 17:44:56 +0800

    javascript file

--- form.js
+++ form.js
@@ -7,18 +7,18 @@ function checkPasswordStrength() {
 		$('#password-strength-status').removeClass();
 		$('#password-strength-status').addClass('weak-password');
 		$('#password-strength-status').html("Weak (should be atleast 6 characters.)");
-		submit.disabled = true;
+		$("#submit").attr("disabled" , true);
 	} else {  	
 	    if($('#password').val().match(number) && $('#password').val().match(alphabets) && $('#password').val().match(special_characters)) {            
 			$('#password-strength-status').removeClass();
 			$('#password-strength-status').addClass('strong-password');
 			$('#password-strength-status').html("Strong");
-			submit.disabled = false;
+			$("#submit").attr("disabled" , false);
         } else {
 			$('#password-strength-status').removeClass();
 			$('#password-strength-status').addClass('medium-password');
 			$('#password-strength-status').html("Medium (should include alphabets, numbers and special characters.)");
-			submit.disabled = true;
+			$("#submit").attr("disabled" , true);
         } 
 	}
 }
@@ -26,8 +26,7 @@ function checkPasswordStrength() {
 function checkEmail() {
 	var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 	if($("#mail").val().match(mailformat)) {
-		Swal.fire("Your email is valid")
-		submit.disabled = false;
+		$("#submit").attr("disabled" , false);
 	}
 	else {
 		Swal.fire({
@@ -35,7 +34,7 @@ function checkEmail() {
 			title: 'Oops...',
 			text: 'Your email is invalid!',
 		})
-		submit.disabled = true;
+		$("#submit").attr("disabled" , true);
 	}
 }
 
